6166f9df2681f7f2c4e9341dde64b9c09397b7e3
Venanciofaf <venanciofaf@al.insper.edu.br>
2025-02-14 19:53:47 -0300

    Código 3 feito

--- exe3/main.c
+++ exe3/main.c
@@ -4,14 +4,15 @@
 
 const int BTN_PIN_R = 28;
 const int BTN_PIN_G = 26;
+volatile uint32_t evento = 0;
+volatile uint gpio_main = 0;
+volatile btnFlag = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) { // fall edge
-    if (gpio == BTN_PIN_R)
-      printf("fall red\n");
-    else if (gpio == BTN_PIN_G)
-      printf("fall green\n");
-  }
+    btnFlag = 1;
+    evento = events;
+    gpio_main = gpio;
+
 }
 
 int main() {
@@ -33,5 +34,14 @@ int main() {
   gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
   while (true) {
+    if (btnFlag){
+      if (evento == 0x4) { // fall edge
+        if (gpio_main == BTN_PIN_R)
+          printf("fall red\n");
+        else if (gpio_main == BTN_PIN_G)
+          printf("fall green\n");
+      }
+      btnFlag = 0;
+    }
   }
 }
